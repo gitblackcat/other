15032b27b631a357ba228cbb3169ac285c749316
Your Name <you@example.com>
2017-07-24 08:16:58 +0800

    update

--- 窗口大小自适应/js适配根字体方法/remS.js
+++ 窗口大小自适应/js适配根字体方法/remS.js
@@ -29,12 +29,12 @@
     if (!metaEl) {
         metaEl = doc.createElement('meta')
         metaEl.setAttribute('name', 'viewport')
-        metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ', maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no')
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no')
 
         docEl.firstElementChild.appendChild(metaEl)
     } else {
         metaEl.setAttribute('name', 'viewport')
-        metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no')
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no')
     }
 
     function refreshRem(designWidth, rem2px) {
